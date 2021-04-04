24ada22ea08824c612f7339ae40b6f255c8ecbcb
kubernetes-estudos <michel.torrezani@gmail.com>
2021-04-04 18:18:46 -0300

    Add um arquivo em shell operadores.sh

---
+++ operadores.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+clear;
+###### Operadores Aritméticos ######
++ (soma)
+- (subtração)
+* (multiplicação)
+/ (divisão)
+% (resto da divisão)
+
+###### Operadores Lógicos ######
+&& (AND)
+|| (OR)
+!  (NOT)
+
+###### Comparadores ######
+-eq (igual)
+-ne (diferente)
+-gt (maior)
+-lt (menor)
+-le (menor igual)
+-ge (maior igual)
+
+
